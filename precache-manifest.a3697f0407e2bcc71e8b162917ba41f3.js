4c4d73dbc20f23742981cadcf738f7440a446854
hzwangzhenhua <hzwangzhenhua@corp.netease.com>
2020-08-02 22:12:22 +0800

    Updates

---
+++ precache-manifest.a3697f0407e2bcc71e8b162917ba41f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389e4d1771f7c08b3f5fa3cf8e5cd7b5",
+    "url": "/sql2ts/index.html"
+  },
+  {
+    "revision": "ab076c1a667a6548e98a",
+    "url": "/sql2ts/static/css/main.3d7566eb.chunk.css"
+  },
+  {
+    "revision": "597b51c0f569ec95cace",
+    "url": "/sql2ts/static/js/2.554a1d37.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sql2ts/static/js/2.554a1d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab076c1a667a6548e98a",
+    "url": "/sql2ts/static/js/main.655107c2.chunk.js"
+  },
+  {
+    "revision": "135501306e50da8f6192",
+    "url": "/sql2ts/static/js/runtime-main.56c3b1d9.js"
+  }
+]);
